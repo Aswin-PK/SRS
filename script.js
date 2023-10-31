cc6f090151b4260356d9672549e0dade9af86147
Aswin P K <97808507+Aswin-PK@users.noreply.github.com>
2023-11-01 00:14:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// FOR IMAGE SLIDER 
 $(document).ready(function(){
     $('.slider').each(function(){
     $(this).slick({
@@ -8,23 +9,36 @@ $(document).ready(function(){
         autoplaySpeed: 5000, // Autoplay speed in milliseconds (adjust as needed)
         dots: false, // Show navigation dots
         arrows: false, // Show navigation arrows
+        swipe: false, // restrict manual swipe scroll
     });
     });
 });
 
+// FOR NAVBAR MENU ICON FOR SMALLER SCREEN
+document.addEventListener('click', function(event) {
+    const dropdown = document.getElementById('dropdownDivider');
+    
+    // Check if the clicked element is not within the dropdown
+    if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove('hidden');
+        dropdown.classList.remove('absolute');
+    }
+});
 function handleClick(){
     document.getElementById('dropdownDivider').classList.toggle('hidden')
     document.getElementById('dropdownDivider').classList.toggle('absolute')
 }
 
+
+// FOR GETTING SCROLL VALUE FOR DISPLAYING NAVBAR
 window.onscroll = function(){
     scrollfunction();
 }
 function scrollfunction(){
-  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
     document.getElementById("navbar").classList.remove('hidden');
-  }
-  else{
+}
+else{
     document.getElementById("navbar").classList.add('hidden');
-  }
+}
 }
